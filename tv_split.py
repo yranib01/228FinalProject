1c872a3cb9376e10354ae4fcda9b05d6fa91347d
yranib01 <rypai@ucsd.edu>
2025-05-12 14:18:45 -0700

    new files

--- tv_split.py
+++ tv_split.py
@@ -2,14 +2,23 @@ from load_dataset import *
 import torch
 
 class CustomDataset(torch.utils.data.Dataset):
-    def __init__(self, x, y)
+    def __init__(self, x, y):
         self.x = x
         self.y = y
 
+    def __getitem__(self, idx):
+        sample = {
+            "features": torch.tensor(self.x[idx], dtype=torch.float),
+            "labels": torch.tensor(self.y[idx], dtype=torch.float)
+        }
+
+    def __len__(self):
+        return len(self.x)
 
 
+my_dataset = CustomDataset(split, ys)
+
+train_data, val_data = torch.utils.data.random_split(my_dataset, [0.8, 0.2])
 
 
-
-my_datset = CustomDataset(split, ys)
 
